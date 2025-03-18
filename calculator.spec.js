89bed16b55e5d4a07401282a55e1280574b89e45
Mark Guhathakurta-Halliday <133343189+WebTestMan@users.noreply.github.com>
2025-03-18 12:32:01 +0000

    Added unit tests

--- calculator.spec.js
+++ calculator.spec.js
@@ -1,38 +1,37 @@
-// const {convertToCelsius, convertToFahrenheit} = require('./tempConversion')
-const { add, subtract, multiply, divide } = require("./calculator");
+const calculator = require('./calculator');
 
 describe("add", () => {
   test("works", () => {
-    expect(add(10, 2)).toEqual(12);
+    expect(calculator.add(10, 2)).toEqual(12);
   });
   test("works", () => {
-    expect(add(8, 8)).toEqual(16);
+    expect(calculator.add(8, 8)).toEqual(16);
   });
 });
 
 describe("substract", () => {
   test("works", () => {
-    expect(subtract(2, 1)).toEqual(2);
+    expect(calculator.subtract(2, 1)).toEqual(1);
   });
   test("works", () => {
-    expect(subtract(10, 1)).toEqual(9);
+    expect(calculator.subtract(10, 1)).toEqual(9);
   });
 });
 
 describe("multiply", () => {
   test("works", () => {
-    expect(multiply(2, 1)).toEqual(2);
+    expect(calculator.multiply(2, 1)).toEqual(2);
   });
   test("works", () => {
-    expect(multiply(2, 2)).toEqual(4);
+    expect(calculator.multiply(2, 2)).toEqual(4);
   });
 });
 
 describe("divide", () => {
   test("works", () => {
-    expect(divide(4, 2)).toEqual(2);
+    expect(calculator.divide(4, 2)).toEqual(2);
   });
   test("works", () => {
-    expect(divide(16, 2)).toEqual(8);
+    expect(calculator.divide(16, 2)).toEqual(8);
   });
 });
